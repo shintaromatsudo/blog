31b7b44680ae980d49eca4ac818e9b797abf09ab
shintaro matsudo <shintaro011224@gmail.com>
2020-02-17 13:41:36 +0900

    set url

--- content/pages/blog.md
+++ content/pages/blog.md
@@ -1,6 +1,6 @@
 ---
 template: BlogIndex
-slug: blog
+slug: ''
 title: Misara Blog
 featuredImage: 'https://ucarecdn.com/b6254577-26bf-4b79-aa9b-f3793e2aebdc/'
 subtitle: プログラミング×旅
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,10 @@ import React from 'react'
 import CMS from 'netlify-cms-app'
 import './cms-utils'
 
-import { HomePageTemplate } from '../templates/HomePage'
-import { ComponentsPageTemplate } from '../templates/ComponentsPage'
+// import { HomePageTemplate } from '../templates/HomePage'
+// import { ComponentsPageTemplate } from '../templates/ComponentsPage'
 import { ContactPageTemplate } from '../templates/ContactPage'
-import { DefaultPageTemplate } from '../templates/DefaultPage'
+// import { DefaultPageTemplate } from '../templates/DefaultPage'
 import { BlogIndexTemplate } from '../templates/BlogIndex'
 import { SinglePostTemplate } from '../templates/SinglePost'
 
@@ -20,18 +20,18 @@ if (
   CMS.registerPreviewStyle('/styles.css')
 }
 
-CMS.registerPreviewTemplate('home-page', ({ entry }) => (
-  <HomePageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('components-page', ({ entry }) => (
-  <ComponentsPageTemplate {...entry.toJS().data} />
-))
+// CMS.registerPreviewTemplate('home-page', ({ entry }) => (
+//   <HomePageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('components-page', ({ entry }) => (
+//   <ComponentsPageTemplate {...entry.toJS().data} />
+// ))
 CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
   <ContactPageTemplate {...entry.toJS().data} />
 ))
-CMS.registerPreviewTemplate('infoPages', ({ entry }) => (
-  <DefaultPageTemplate {...entry.toJS().data} />
-))
+// CMS.registerPreviewTemplate('infoPages', ({ entry }) => (
+//   <DefaultPageTemplate {...entry.toJS().data} />
+// ))
 CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
   <BlogIndexTemplate {...entry.toJS().data} />
 ))
