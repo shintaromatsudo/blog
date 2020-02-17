65369b47708d36028190a4d4295e2c9068b52342
shintaro matsudo <shintaro011224@gmail.com>
2020-02-17 12:02:58 +0900

    yellowcake to misarablog

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com',
+    title: 'MisaraBlog',
+    siteUrl: 'https://misara-2020.netlify.com',
     description: '説明',
   },
   plugins: [
@@ -45,8 +45,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'misarablog',
+        short_name: 'misarablog',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
