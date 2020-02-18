c3f747e7d6e51850222ec22ca5b854f0278ae30e
shintaro matsudo <shintaro011224@gmail.com>
2020-02-18 22:30:19 +0900

    form10

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -66,7 +66,7 @@ class Form extends React.Component {
           name={name}
           action={action}
           data-netlify="true"
-          data-netlify-honeypot="bot-field"
+          data-netlify-honeypot="_gotcha"
           netlify-recaptcha="true"
           onSubmit={this.handleSubmit}
         >
